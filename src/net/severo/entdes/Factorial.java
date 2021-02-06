ae8f718c765dfeae4d85ee2ea62754fe558927b4
Alejandro Sancho <34617@DESKTOP-QUD65H0>
2021-02-06 16:25:17 +0100

    Cambio en variable.

--- src/net/severo/entdes/Factorial.java
+++ src/net/severo/entdes/Factorial.java
@@ -9,10 +9,11 @@ public class Factorial {
 	public static void main(String[] args) {
 
 		int n = (int) (Math.random() * 10);
-		int result = 1;
+		int result = n;
 		System.out.println("Value of n: " + n);
 		while (n > 1) {
 			result = result * (n - 1);
+			n--;
 		}
 		System.out.println("Value of n!: " + result);
 
